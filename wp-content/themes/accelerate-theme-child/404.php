d62723a7a7a4dfabe88327cd3a7d0a5af78cee31
JJana <jana@jjakobs.com>
2020-03-19 11:01:48 +0200

    problems with child theme

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */?>
+
+<div class="fourofour">
+ <h1> 404 Error</h1>
+</div>
